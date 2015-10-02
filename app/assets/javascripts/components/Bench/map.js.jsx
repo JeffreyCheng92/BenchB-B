a54acacdc1d79dacfab54744afa9b0bce95c5efa
Jeffrey Cheng <JeffreyCheng92@gmail.com>
2015-10-01 18:07:57 -0700

    added markers for benches on map

--- app/assets/javascripts/components/Bench/map.js.jsx
+++ app/assets/javascripts/components/Bench/map.js.jsx
@@ -1,6 +1,11 @@
 var BenchMap = React.createClass({
+  getInitialState: function() {
+    return { benches: BenchStore.all() };
+  },
 
   componentDidMount: function(){
+    BenchStore.addChangeListener(this._onChange);
+
     var map = React.findDOMNode(this.refs.map);
     var mapOptions = {
       center: {lat: 37.7538, lng: -122.435},
@@ -9,7 +14,22 @@ var BenchMap = React.createClass({
     this.map = new google.maps.Map(map, mapOptions);
   },
 
+  _onChange: function() {
+    this.setState( { benches: BenchStore.all() });
+  },
+
   render: function() {
+    var marker;
+
+    this.state.benches.forEach(function(bench) {
+      marker = new google.maps.Marker({
+        position: {lat: bench.lat, lng: bench.lng},
+        title: bench.description
+      });
+
+      marker.setMap(this.map);
+    }.bind(this));
+
     return(
       <div className="map" ref="map"></div>
     );
