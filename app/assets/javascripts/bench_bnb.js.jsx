cc6b76ed11b55842b330b41642d32765109b496e
Jeffrey Cheng <JeffreyCheng92@gmail.com>
2015-10-02 14:47:31 -0700

    new bench form component

--- app/assets/javascripts/bench_bnb.js.jsx
+++ app/assets/javascripts/bench_bnb.js.jsx
@@ -1,4 +1,23 @@
 $(function() {
-  var content = document.getElementById("content");
-  React.render(<Search/>, content);
+  var root = document.getElementById('content');
+  var Route = ReactRouter.Route;
+  var Router = ReactRouter.Router;
+  var IndexRoute = ReactRouter.IndexRoute;
+
+  var App = React.createClass({
+    render: function(){
+      return (
+        <div>
+          <header><h1>Bench BnB</h1></header>
+          {this.props.children}
+        </div>
+      );
+    }
+  });
+  var routes = (
+      <Route path="/" component={App}>
+        <IndexRoute component={Search}/>
+      </Route>
+  );
+  React.render(<Router>{routes}</Router>, root);
 });
---
+++ app/assets/javascripts/components/Bench/form.js.jsx
@@ -0,0 +1,23 @@
+var BenchForm = React.createClass({
+  render: function() {
+    return(
+      <div>
+        <form>
+          <label>Description</label>
+          <br>
+          <textarea rows="4" cols ="50"></textarea>
+          <br>
+          <label> Latitude </label>
+          <br>
+          <input type='text'/>
+          <br>
+          <label> Longitude </label>
+          <input type='text'/>
+          <br>
+          <input type="submit" value='submit'/>
+        </form>
+      </div>
+    )
+  }
+
+});
