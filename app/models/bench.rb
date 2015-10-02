7ad620362fae32db0ca9ec05a5c7484def557259
Jeffrey Cheng <JeffreyCheng92@gmail.com>
2015-10-01 18:47:02 -0700

    added query method for benches

--- app/models/bench.rb
+++ app/models/bench.rb
@@ -12,4 +12,18 @@
 
 class Bench < ActiveRecord::Base
   validates :description, :lat, :lng, presence: true
+
+  def self.in_bounds(bounds)
+    # bounds = {
+    #   "northEast"=> {"lat"=>"37.80971", "lng"=>"-122.39208"},
+    #   "southWest"=> {"lat"=>"37.74187", "lng"=>"-122.47791"}
+    # }
+
+    benches = Bench.where("lat >= ? AND lat <= ? AND lng >= ? AND lng <= ?",
+                bounds["southWest"]["lat"].to_f,
+                bounds["northEast"]["lat"].to_f,
+                bounds["southWest"]["lng"].to_f,
+                bounds["northEast"]["lng"].to_f )
+    return benches
+  end
 end
